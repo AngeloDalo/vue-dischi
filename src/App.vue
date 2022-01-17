b61aab94474a8e55e6e3779d34d3ef089c9eeb69
AngeloDalo <angelo251158@gmail.com>
2022-01-17 14:16:03 +0100

    axios

--- src/App.vue
+++ src/App.vue
@@ -20,6 +20,7 @@ export default {
 
 <style lang="scss">
 @import "./assets/scss/style.scss";
+@import "~bootstrap/scss/bootstrap";
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -5,13 +5,25 @@
 </template>
 
 <script>
+import axios from 'axios';
 export default {
     name: 'Main',
     data () {
         return {
-
+            cardMusic: null,
         }
-    }
+    },
+    mounted() {
+            axios
+            .get("https://flynn.boolean.careers/exercises/api/array/music")
+            .then((response) => {
+                this.cardMusic = response.data.response;
+                console.log(this.cardMusic);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    },
 }
 </script>
 
